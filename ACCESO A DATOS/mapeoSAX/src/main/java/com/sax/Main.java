283edf7ad2f9e1a186e67113cbd264a19f3dad2a
hector-rodri <hheeccttoorr2005@gmail.com>
2025-10-21 17:46:16 +0200

    comments

--- ACCESO A DATOS/mapeoSAX/src/main/java/com/sax/Main.java
+++ ACCESO A DATOS/mapeoSAX/src/main/java/com/sax/Main.java
@@ -1,22 +1,19 @@
 package com.sax;
 
-import java.io.*;
+import java.io.*;//Import libraries
 import javax.xml.parsers.*;
 import org.xml.sax.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-
-            SAXParser parser = factory.newSAXParser();
-
-            Player handler = new Player();
-            parser.parse("recursos/players.xml", handler);
+            SAXParserFactory factory = SAXParserFactory.newInstance();//Create a new SAXParserFactory instance
+            SAXParser parser = factory.newSAXParser();//Use the factory to create a new SAXParser
+            Player handler = new Player();//Create an instance of the our handler
+            parser.parse("recursos/players.xml", handler);//Parse the XML file using the handler
 
         } catch (ParserConfigurationException | SAXException | IOException ex) {
             System.out.println(ex.getMessage());
         }
-
     }
 }
--- ACCESO A DATOS/mapeoSAX/src/main/java/com/sax/Player.java
+++ ACCESO A DATOS/mapeoSAX/src/main/java/com/sax/Player.java
@@ -1,54 +1,54 @@
 package com.sax;
 
-import org.xml.sax.*;
+import org.xml.sax.*;//Import libraries
 import org.xml.sax.helpers.*;
 import java.io.*;
 
-public class Player extends DefaultHandler {
+public class Player extends DefaultHandler {//Class that can handle XML events by extending DefaultHandler
 
-    private StringBuilder value;
-    private String playerText = "";
-    private int counter = 0;
+    private StringBuilder value;//Store text from XML elements
+    private String playerText = "";//Store the info player by player for write in the file
+    private int counter = 0;//Counter for name files
 
     public Player() {
-        this.value = new StringBuilder();
+        this.value = new StringBuilder();//Creates a empty StringBuilder
     }
 
     @Override
-    public void characters(char ch[], int start, int length) throws SAXException {
-        this.value.setLength(0);
-        this.value.append(ch, start, length);
+    public void characters(char ch[], int start, int length) throws SAXException {//Method called when the parser reads text inside an XML element
+        this.value.setLength(0);//Clear the current content of the StringBuilder
+        this.value.append(ch, start, length);//Append new characters from the XML to value
     }
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
+    public void endElement(String uri, String localName, String qName) throws SAXException {//Method called when the parser read the end of an XML element
         switch (qName) {
-            case "Player":
+            case "Player"://If the element is Player then print blank line and creates a file with player content 
                 System.out.println("");
                 try {
                     counter++;
-                    File file = new File("player" + counter + ".txt");
-                    FileWriter writer = new FileWriter(file);
-                    writer.write(playerText);
+                    File file = new File("player" + counter + ".txt");//Creates a new file
+                    FileWriter writer = new FileWriter(file);//Creates a new file writer for write into the file
+                    writer.write(playerText);//Writes into the file the String playerText with the player info 
                     writer.close();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                playerText = "";
+                playerText = "";//Clean the var for store new info of another player
                 break;
-            case "Name":
+            case "Name"://If the element is Name, print the name value and save it in playerText
                 System.out.println("Name: " + this.value.toString());
                 playerText += "Name: " + this.value.toString() + "\n";
                 break;
-            case "Position":
+            case "Position"://If the element is Position, print the position value and save it in playerText
                 System.out.println("Position: " + this.value.toString());
                 playerText += "Position: " + this.value.toString() + "\n";
                 break;
-            case "Nationality":
+            case "Nationality"://If the element is Nationality, print the nationality value and save it in playerText
                 System.out.println("Nationality: " + this.value.toString());
                 playerText += "Nationality: " + this.value.toString() + "\n";
                 break;
-            case "Club":
+            case "Club"://If the element is Club, print the club value and save it in playerText
                 System.out.println("Club: " + this.value.toString());
                 playerText += "Club: " + this.value.toString() + "\n";
                 break;
