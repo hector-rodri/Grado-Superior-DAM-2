648dca63e8170cd6c64c9f3cfb52daaf53eb5ba6
hector-rodri <hrodriguez@nigul.cide.es>
2025-11-06 15:19:26 +0100

    final version

--- PROGRAMACION MULTIMEDIA/planDeEntrenamiento/app/src/main/java/com/example/plandeentrenamiento/MainActivity.kt
+++ PROGRAMACION MULTIMEDIA/planDeEntrenamiento/app/src/main/java/com/example/plandeentrenamiento/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.plandeentrenamiento
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.widget.EditText
-import android.content.Intent
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,18 +18,31 @@ class MainActivity : AppCompatActivity() {
         val editTxt = findViewById<EditText>(R.id.editTextText)
         val editTxt2 = findViewById<EditText>(R.id.editTextText2)
         val btnCreate = findViewById<Button>(R.id.button)
+        val textResult = findViewById<TextView>(R.id.textView)
 
-        btnCreate.setOnClickListener{
-            val editTxtValue = editTxt.text.toString()
-            val editTxtValue2 = editTxt2.text.toString()
-            if (editTxtValue.isEmpty() or editTxtValue2.isEmpty()){
-                Error("The fields mustn't be empty")
-            }else{
-                val intent =  Intent(this, MainActivity2::class.java)
-                intent.putExtra("value1",editTxtValue)
-                intent.putExtra("value2",editTxtValue2)
-                startActivity(intent)
+        val launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val resum = result.data?.getStringExtra("resum")
+                textResult.text = resum
+            } else {
+                textResult.text = "Operació cancel·lada"
             }
+        }
+
+        btnCreate.setOnClickListener {
+            val txt1 = editTxt.text.toString()
+            val txt2 = editTxt2.text.toString()
+            val intent = Intent(this, MainActivity2::class.java)
+
+            if (txt1.isEmpty() || txt2.isEmpty()) {
+                Error( "Els camps no poden estar buits")
+            }
+
+            intent.putExtra("value1", txt1.toInt())
+            intent.putExtra("value2", txt2.toInt())
+            launcher.launch(intent)
         }
     }
 }
--- PROGRAMACION MULTIMEDIA/planDeEntrenamiento/app/src/main/java/com/example/plandeentrenamiento/MainActivity2.kt
+++ PROGRAMACION MULTIMEDIA/planDeEntrenamiento/app/src/main/java/com/example/plandeentrenamiento/MainActivity2.kt
@@ -1,10 +1,11 @@
 package com.example.plandeentrenamiento
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,26 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
 
-        val value1 = intent.getStringExtra("value1")
+        val txtResum = findViewById<TextView>(R.id.textViewSummary)
+        val btnConfirm = findViewById<Button>(R.id.buttonConfirm)
+        val btnCancell = findViewById<Button>(R.id.buttonCancel)
+        val days = intent.getIntExtra("value1", 0)
+        val weeks = intent.getIntExtra("value2", 0)
+        val sessions = days * weeks
+        val resum = "Has seleccionat un entrenament de $days dies per setmana, " + "durant $weeks setmanes ($sessions sessions)"
 
+        txtResum.text = resum
 
+        btnConfirm.setOnClickListener {
+            val data = Intent()
+            data.putExtra("resum", resum)
+            setResult(RESULT_OK, data)
+            finish()
+        }
 
+        btnCancell.setOnClickListener {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
     }
 }
