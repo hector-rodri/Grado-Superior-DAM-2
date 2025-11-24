2fadc60f6398191e33259308259aa533fceff5e2
hector-rodri <hrodriguez@nigul.cide.es>
2025-11-24 22:45:48 +0100

    managechefs method

--- ACCESO A DATOS/Database Connections/restaurantObjectDB/src/main/java/com/example/Main.java
+++ ACCESO A DATOS/Database Connections/restaurantObjectDB/src/main/java/com/example/Main.java
@@ -1,74 +1,46 @@
 package com.example;
 
 import javax.persistence.*;
-
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("objectdb_restaurant");
         EntityManager em = emf.createEntityManager();
 
-        // while (true) {}
-        System.out.println("Welcome to the Cooking School Management System!");
-        System.out.println("Choose an option:");
-        System.out.println("1 - Manage chefs");
-        System.out.println("2 - Manage courses");
-        System.out.println("3 - Manage students");
-        System.out.println("4 - Manage recipes");
-        System.out.println("5 - Exit");
-        int choice = sc.nextInt();
+        while (true) {
+            System.out.println("Welcome to the Cooking School Management System!");
+            System.out.println("Choose an option:");
+            System.out.println("1 - Manage chefs");
+            System.out.println("2 - Manage courses");
+            System.out.println("3 - Manage students");
+            System.out.println("4 - Manage recipes");
+            System.out.println("5 - Exit");
+            int choice = sc.nextInt();
 
-        switch (choice) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                System.out.println("Exiting the system...");
-                return;
-            default:
-                System.out.println("Invalid option");
-        }
-
-        try {
-            em.getTransaction().begin();
-
-            Chef chef1 = new Chef("Hector Rodriguez", "Fish Dishes");
-            Chef chef2 = new Chef("Jaume Salas", "Meat Dishes");
-            Course course1 = new Course("Seafood", 2);
-            Course course2 = new Course("Meat", 1);
-            Recipe recipe1 = new Recipe("Salmon", "Medium");
-            Recipe recipe2 = new Recipe("Beef Steak", "Hard");
-            Student student1 = new Student("Alberto");
-            Student student2 = new Student("Elena");
-
-            chef1.addCourse(course1);
-            chef2.addCourse(course2);
-            course1.addRecipe(recipe1);
-            course2.addRecipe(recipe2);
-            course1.addStudent(student1);
-            course2.addStudent(student2);
-
-            em.persist(chef1);
-            em.persist(chef2);
-
-            em.getTransaction().commit();
-
-        } finally {
-            em.close();
-            emf.close();
+            switch (choice) {
+                case 1:
+                    manageChefs(em);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Exiting the system...");
+                    em.close();
+                    emf.close();
+                    return;
+                default:
+                    System.out.println("Invalid option");
+            }
         }
     }
 
-    private static void manageChefs() {
+    private static void manageChefs(EntityManager em) {
         while (true) {
             System.out.println("--Manage Chefs--");
             System.out.println("1 - Add chef");
@@ -77,25 +49,70 @@ public class Main {
             System.out.println("4 - Delete chef");
             System.out.println("5 - Back");
             int choice = sc.nextInt();
+            sc.nextLine();
 
             switch (choice) {
                 case 1:
-
+                    System.out.print("Enter chef name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter specialty: ");
+                    String specialty = sc.nextLine();
+                    em.getTransaction().begin();
+                    Chef chef = new Chef(name, specialty);
+                    em.persist(chef);
+                    em.getTransaction().commit();
+                    if (chef.getId() != 0) {
+                        System.out.println("Chef added");
+                    }else {
+                        System.out.println("Error adding chef");
+                    }
                     break;
                 case 2:
-
+                    List<Chef> chefs = em.createQuery("SELECT c FROM Chef c", Chef.class).getResultList();
+                    for (Chef c : chefs) {
+                        System.out.println(c.getId() + " - " + c.getName() + " (" + c.getSpecialty() + ")");
+                    }
                     break;
                 case 3:
+                    System.out.print("Enter chef ID to update: ");
+                    long idUpdate = sc.nextLong();
+                    sc.nextLine();
+                    Chef chefToUpdate = em.find(Chef.class, idUpdate);
+                    if (chefToUpdate != null) {
+                        System.out.print("Enter new name: ");
+                        String newName = sc.nextLine();
+                        System.out.print("Enter new specialty: ");
+                        String newSpecialty = sc.nextLine();
 
+                        em.getTransaction().begin();
+                        chefToUpdate.setName(newName);
+                        chefToUpdate.setSpecialty(newSpecialty);
+                        em.getTransaction().commit();
+                        System.out.println("Chef updated successfully!");
+                    } else {
+                        System.out.println("Chef not found.");
+                    }
                     break;
-                case 4:
 
+                case 4:
+                    System.out.print("Enter chef ID to delete: ");
+                    long idDelete = sc.nextLong();
+                    sc.nextLine();
+
+                    Chef chefToDelete = em.find(Chef.class, idDelete);
+                    if (chefToDelete != null) {
+                        em.getTransaction().begin();
+                        em.remove(chefToDelete);
+                        em.getTransaction().commit();
+                        System.out.println("Chef deleted successfully!");
+                    } else {
+                        System.out.println("Chef not found.");
+                    }
                     break;
                 case 5:
                     return;
                 default:
                     System.out.println("Invalid option");
-
             }
         }
     }
