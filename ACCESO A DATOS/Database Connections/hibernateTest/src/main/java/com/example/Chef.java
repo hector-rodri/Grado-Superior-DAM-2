d7a176451c3d4cab99c65cf69dcfc9a14bde0d2f
hector-rodri <hrodriguez@nigul.cide.es>
2025-12-18 21:46:07 +0100

    add project

---
+++ ACCESO A DATOS/Database Connections/hibernateTest/src/main/java/com/example/Chef.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "chef")
+public class Chef {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "specialty", length = 100)
+    private String specialty;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+}
---
+++ ACCESO A DATOS/Database Connections/hibernateTest/src/main/java/com/example/Student.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+}
