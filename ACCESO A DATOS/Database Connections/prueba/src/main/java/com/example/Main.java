eda167f676f4d48a8bca539d425d23ef75693c2e
hector-rodri <hrodriguez@nigul.cide.es>
2025-12-18 18:12:27 +0100

    fix

--- ACCESO A DATOS/Database Connections/prueba/src/main/java/com/example/Main.java
+++ ACCESO A DATOS/Database Connections/prueba/src/main/java/com/example/Main.java
@@ -1,24 +1,7 @@
 package com.example;
 
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 public class Main {
     public static void main(String[] args) {
-
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-
-        Empleado emp = new Empleado();
-        emp.setNombre("Juan");
-        emp.setPuesto("IT");
-
-        session.persist(emp); 
-
-        tx.commit();
-        session.close();
-
-        System.out.println("Empleado guardado");
+        System.out.println("Hola mundo");
     }
 }
