270c25fbbc7e5d1198241bd7dc90a18091cfece4
hector-rodri <hrodriguez@nigul.cide.es>
2026-01-22 18:20:15 +0100

    print cores

--- SERVICIOS & PROCESOS/Threads/ThreadsPerCore/src/main/java/com/example/Main.java
+++ SERVICIOS & PROCESOS/Threads/ThreadsPerCore/src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         int cores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Cores: " + cores);
 
         for (int i = 0; i < cores; i++) {
             int threadId = i;
