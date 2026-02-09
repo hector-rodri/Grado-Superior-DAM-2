093f96a798e69e1429ae38f81196e75aa4603dda
hector-rodri <hheeccttoorr2005@gmail.com>
2026-02-09 18:30:09 +0100

    Add initial microservices CRUD structure with Maven and Spring Boot configuration

---
+++ ACCESO A DATOS/mircroservicesCRUD/src/main/java/com/example/MircroservicescrudApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircroservicescrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MircroservicescrudApplication.class, args);
+	}
+
+}
